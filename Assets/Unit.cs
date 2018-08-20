fcf9834362bb58179c3a07242cff5d8143de7411
Sam Sobell <sam.sobell@gmail.com>
2018-08-20 08:26:57 -0400

    Added unit class, removed script that caused warnings

---
+++ Assets/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : Actor {
+
+    public int maxHealth;
+    public int currentHealth;
+
+    public int armor;
+    public int magicResist;
+
+    public int strength;
+    public int agility;
+    public int intelligence;
+
+}
